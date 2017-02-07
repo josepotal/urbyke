88c163b2cd86a37f2e589fdf7252d42381ac0295
Ernesto Parra <ernestoparra@MacBook-Pro-de-Ernesto.local>
2017-02-07 17:03:53 +0100

    markers indicators

--- js/controllers/LocationController.js
+++ js/controllers/LocationController.js
@@ -1,19 +1,22 @@
 /* eslint no-undef: "off" */
 angular.module('urbykeApp')
-.controller('LocationController', function (BikeFactory, apikey, NgMap) {
-	var vm = this 
-	vm.googleMapsUrl = 'https://maps.googleapis.com/maps/api/js?key=' + apikey
+    .controller('LocationController', function(BikeFactory, apikey, NgMap, $rootScope) {
 
-	BikeFactory.urbykeCall()
-	.then(function(response) {
-		vm.latitude = response.data.network.location.latitude
-		vm.longitude = response.data.network.location.longitude
-		vm.stations = response.data.network.stations
-		console.log(vm.stations[0])
-	})
+    	var latAndLon
+        var vm = this
+        vm.googleMapsUrl = 'https://maps.googleapis.com/maps/api/js?key=' + apikey
 
+        BikeFactory.urbykeCall()
+            .then(function(response) {
+                vm.latitude = response.data.network.location.latitude
+                vm.longitude = response.data.network.location.longitude
+                vm.stations = response.data.network.stations
+                $rootScope.latAndLon = vm.stations.map(function(elem) {
+            	return elem.latitude +", "+ elem.longitude
 
-
-
-
-})
+            })
+                // console.log($rootScope.latAndLon)
+      
+            })
+            
+    })
