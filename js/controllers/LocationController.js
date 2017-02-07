51a8db12b19703d2182fad396689bb659189dcea
FransLopez <frans.lopez.m@gmail.com>
2017-02-07 10:54:19 +0100

    LocationController modified

--- js/controllers/LocationController.js
+++ js/controllers/LocationController.js
@@ -4,10 +4,4 @@ angular.module('urbykeApp')
   var vm = this
   vm.title = 'Winter is coming'
   vm.googleMapsUrl = 'https://maps.googleapis.com/maps/api/js?key=' + apikey
-  console.log(vm.googleMapsUrl)
-  // NgMap.getMap().then(function (map) {
-  //   console.log(map.getCenter())
-  //   console.log('markers', map.markers)
-  //   console.log('shapes', map.shapes)
-  // })
 })
