4f7d1964597fc4867eeb0f03fb23d65d75fc9dcc
FransLopez <frans.lopez.m@gmail.com>
2017-02-07 13:10:57 +0100

    main.html and index.html style modified

--- js/controllers/LocationController.js
+++ js/controllers/LocationController.js
@@ -2,6 +2,5 @@
 angular.module('urbykeApp')
 .controller('LocationController', function (BikeFactory, apikey, NgMap) {
   var vm = this
-  vm.title = 'Winter is coming'
   vm.googleMapsUrl = 'https://maps.googleapis.com/maps/api/js?key=' + apikey
 })
