3237a6022d5840df16450a519c6ca983342b731a
Josep Otal <j.otal.pares@gmail.com>
2017-02-08 01:16:53 +0100

    josep tests on infostation window

--- js/controllers/LocationController.js
+++ js/controllers/LocationController.js
@@ -1,5 +1,28 @@
 /* eslint no-undef: "off" */
 angular.module('urbykeApp')
 .controller('LocationController', function (BikeFactory, NgMap) {
-  var vm = this
-})
+  var vm = this;
+
+  NgMap.getMap().then(function(map) {
+    var map = map;
+    console.log(map.getCenter());
+    console.log('markers', map.markers);
+    console.log('shapes', map.shapes);
+  });
+
+  
+ BikeFactory.getBikeStations()
+ .then(function(response) {
+  vm.info = response
+  console.log(vm.info)
+  
+ })
+  // .then(function(response){
+  //   vm.stations = response.data.network.stations
+  //   console.log(vm.stations)
+  // })
+
+  vm.info1 = {id:"111", address:"Plaça Catalunya", slots: 8, bikes:13}
+  vm.station2 = 'Station2'
+
+});
--- js/factories/BikeFactory.js
+++ js/factories/BikeFactory.js
@@ -1,7 +1,28 @@
 /* eslint no-undef: "off" */
 angular.module('urbykeApp')
+
 .factory('BikeFactory', function ($http) {
-  var url = ''
+  var vm = this;
+  function getBikeStations() {
+  var url = 'http://api.citybik.es/v2/networks/bicing'  
+  return $http.get(url)
+  .then(function(response){
+    vm.stations = response.data.network.stations[0]
+    console.log(vm.stations)
+    vm.stationsInfo = {
+      id: vm.stations.id,
+      name: vm.stations.name,
+      bikes: vm.stations.free_bikes,
+      slots: vm.stations.empty_slots, 
+      latitude: vm.stations.latitude,
+      longitude: vm.stations.longitude
+    }
+    console.log(vm.stationsInfo)
+    return vm.stationsInfo
+  })
+}
+
   return {
+    getBikeStations: getBikeStations
   }
 })
