d1b45983528f4605282225818f2e828cc0ec8c97
FransLopez <frans.lopez.m@gmail.com>
2017-02-10 12:57:55 +0100

    pagination resolved

--- js/controllers/StationsListController.js
+++ js/controllers/StationsListController.js
@@ -9,15 +9,15 @@
     BikeFactory.getBikeStations()
       .then(function (response) {
         vm.allStationsInfo = response
+        vm.hasMoreItemsToShow = hasMoreItemsToShow
       })
 // Pagination functionality
     var pagesShown = 1
     var pageSize = 5
-
     vm.paginationLimit = function (data) {
       return pageSize * pagesShown
     }
-    vm.hasMoreItemsToShow = function () {
+    function hasMoreItemsToShow () {
       return pagesShown < (vm.allStationsInfo.length / pageSize)
     }
     vm.showMoreItems = function () {
