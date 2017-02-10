2d8eee06de0b680d7394419a63814e5c8056b686
FransLopez <frans.lopez.m@gmail.com>
2017-02-10 14:59:24 +0100

    link to nearby stations

--- js/factories/BikeFactory.js
+++ js/factories/BikeFactory.js
@@ -7,27 +7,6 @@
     function getBikeStations () {
       var url = 'https://api.citybik.es/v2/networks/bicing'
 
-      function getResults (response) {
-        return response.data.network.stations
-      }
-
-      function getStations (stations) {
-        return stations.map(function (elem) {
-          var stationsInfo = {
-            id: elem.id,
-            name: elem.name,
-            bikes: elem.free_bikes,
-            slots: elem.empty_slots,
-            latitude: elem.latitude,
-            longitude: elem.longitude,
-            nearByStation: elem.extra.NearbyStationList.map(function (elem) {
-              return elem
-            })
-          }
-          return stationsInfo
-        })
-      }
-
       return $http.get(url)
             .then(getResults)
             .then(getStations)
@@ -37,7 +16,7 @@
       return getBikeStations()
             .then(function (stations) {
               return stations.filter(function (station) {
-                return station.id === idStation
+                return station.uid === parseInt(idStation)
               })
             })
     }
@@ -46,5 +25,26 @@
       getBikeStations: getBikeStations,
       getStationDetails: getStationDetails
     }
+  }
+// Helper functions to filter result
+  function getResults (response) {
+    return response.data.network.stations
+  }
+  function getStations (stations) {
+    return stations.map(function (elem) {
+      var stationsInfo = {
+        id: elem.id,
+        name: elem.name,
+        bikes: elem.free_bikes,
+        slots: elem.empty_slots,
+        latitude: elem.latitude,
+        longitude: elem.longitude,
+        uid: elem.extra.uid,
+        nearByStation: elem.extra.NearbyStationList.map(function (elem) {
+          return elem
+        })
+      }
+      return stationsInfo
+    })
   }
 })()
