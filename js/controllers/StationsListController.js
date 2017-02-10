15a1430dedfc6c0781044b81fe15085e14194117
FransLopez <frans.lopez.m@gmail.com>
2017-02-10 12:28:43 +0100

    pagination Station List added

--- js/controllers/StationsListController.js
+++ js/controllers/StationsListController.js
@@ -10,7 +10,20 @@
       .then(function (response) {
         vm.allStationsInfo = response
       })
+// Pagination functionality
+    var pagesShown = 1
+    var pageSize = 5
 
+    vm.paginationLimit = function (data) {
+      return pageSize * pagesShown
+    }
+    vm.hasMoreItemsToShow = function () {
+      return pagesShown < (vm.allStationsInfo.length / pageSize)
+    }
+    vm.showMoreItems = function () {
+      pagesShown = pagesShown + 1
+    }
+// Center Station
     vm.centerStation = function (idStation) {
       BikeFactory.getStationDetails(idStation)
       .then(function (response) {
