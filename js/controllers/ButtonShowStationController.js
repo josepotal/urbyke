1bc1205259c6d8ce0f8dd6d46833c8be111c88a5
Josep Otal <j.otal.pares@gmail.com>
2017-02-10 09:15:28 +0100

    toggle button show map-stations

--- js/controllers/ButtonShowStationController.js
+++ js/controllers/ButtonShowStationController.js
@@ -4,9 +4,13 @@
 .controller('ButtonShowStationController', function ($rootScope) {
   $rootScope.showDetails = ''
   $rootScope.showInfo = ''
+  $rootScope.toggle = false
+  $rootScope.toggleText = 'More Stations'
   $rootScope.moreStations = function () {
     $rootScope.showDetails = ($rootScope.showDetails === 'top-reset') ? '' : 'top-reset'
     $rootScope.showInfo = ($rootScope.showInfo === 'show-info') ? '' : 'show-info'
+    $rootScope.toggleText = $rootScope.toggle ? 'More Stations' : 'Show Map' 
+    $rootScope.toggle = !$rootScope.toggle    
   }
 })
 })()
