9a572e459c39a090de7240efc2ca596097a0ad3e
Josep Otal <j.otal.pares@gmail.com>
2017-02-11 12:53:27 +0100

    config as John Papa

--- js/config.js
+++ js/config.js
@@ -1,16 +1,21 @@
 /* eslint no-undef: "off" */
-angular.module('urbykeApp')
-.config(function ($routeProvider) {
-  $routeProvider
-    .when('/', {
-      templateUrl: 'js/templates/stations-list.html',
-      controller: 'StationsListController',
-      controllerAs: 'vm'
-    })
-    .when('/details/:idStation', {
-      templateUrl: 'js/templates/details-station.html',
-      controller: 'DetailsStationController',
-      controllerAs: 'vm'
-    })
-    .otherwise('/')
-})
+angular
+    .module('urbykeApp')
+    .config(config);
+
+function config($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'js/templates/stations-list.html',
+            controller: 'StationsListController',
+            controllerAs: 'vm'
+        })
+
+        .when('/details/:idStation', {
+            templateUrl: 'js/templates/details-station.html',
+            controller: 'DetailsStationController',
+            controllerAs: 'vm'
+        })
+        
+        .otherwise('/');
+    }
